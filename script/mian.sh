648d4c07f7281c2ac7499133b7dba4ce9e0b240b
samyansan <41437406+samyansan@users.noreply.github.com>
2023-03-12 16:16:50 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -3,9 +3,12 @@
 # 下载规则
 curl -o i-1.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_21.txt
 curl -o i-2.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.txt
-curl -o i-3.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt
-curl -o i-4.txt https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall-dns.txt
-curl -o i-5.txt https://raw.githubusercontent.com/samyansan/AdRules-SamYan/main/Rules/adguardfilter.txt
+curl -o i-3.txt https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall-dns.txt
+curl -o i-4.txt https://raw.githubusercontent.com/samyansan/AdRules-SamYan/main/Rules/adguardfilter.txt
+curl -o i-5.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/mod/rules/dns-rules.txt
+curl -o i-6.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/mod/rules/adblock-rules.txt
+curl -o i-7.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/mod/rules/thrid-part-rules.txt
+curl -o i-8.txt https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
