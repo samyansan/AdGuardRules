3cf4a9eebf30cbcd101ea7ceb99b878d49f1312d
samyansan <41437406+samyansan@users.noreply.github.com>
2023-10-27 09:20:26 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -3,13 +3,10 @@
 # 下载规则
 curl -o i-1.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.txt
 curl -o i-2.txt https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall-dns.txt
-curl -o i-3.txt https://gitlab.com/hagezi/mirror/-/raw/main/dns-blocklists/adblock/light.txt
-curl -o i-4.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_21.txt
-curl -o i-5.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_5.txt
-curl -o i-6.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/mod/rules/dns-rules.txt
-curl -o i-7.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/mod/rules/adblock-rules.txt
-curl -o i-8.txt https://raw.githubusercontent.com/hululu1068/AdGuard-Rule/main/rule/regex.txt
-curl -o i-9.txt https://raw.gitmirror.com/lingeringsound/10007_auto/master/adb.txt
+curl -o i-3.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_21.txt
+curl -o i-4.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/mod/rules/dns-rules.txt
+curl -o i-5.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/mod/rules/adblock-rules.txt
+curl -o i-6.txt https://raw.gitmirror.com/lingeringsound/10007_auto/master/adb.txt
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
