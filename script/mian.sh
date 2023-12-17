5bc1f69f7df7c0f401a7dc3130f03f8cd9e7367f
samyansan <41437406+samyansan@users.noreply.github.com>
2023-12-17 11:50:36 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -4,8 +4,9 @@
 curl -o i-1.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.txt
 curl -o i-2.txt https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall-dns.txt
 curl -o i-3.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_21.txt
-curl -o i-4.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_5.txt
-curl -o i-5.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt
+curl -o i-4.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_27.txt
+curl -o i-5.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_38.txt
+curl -o i-6.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_24.txt
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
