cf23f23b264d80c49bf105942c465606c5b071b7
samyansan <41437406+samyansan@users.noreply.github.com>
2024-01-29 11:00:27 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -4,7 +4,7 @@
 curl -o i-1.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.txt
 curl -o i-2.txt https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall-dns.txt
 curl -o i-3.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_5.txt
-curl -o i-4.txt https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall.txt
+curl -o i-4.txt https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt
 curl -o i-5.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/mod/rules/dns-rules.txt
 curl -o i-6.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/mod/rules/adblock-rules.txt
 
