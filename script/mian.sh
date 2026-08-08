4d8d315fe0e3e51ccb4cccc7636cda2d85f9f02c
samyansan <41437406+samyansan@users.noreply.github.com>
2026-08-08 10:15:43 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -4,8 +4,9 @@
 curl -o i-1.txt https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt
 curl -o i-2.txt https://raw.githubusercontent.com/samyansan/AdRules-SamYan/main/Rules/samyansan.txt
 curl -o i-3.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_lite.txt
-curl -o i-4.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.txt
-curl -o i-5.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_5.txt
+curl -o i-4.txt https://filters.adtidy.org/extension/ublock/filters/224.txt
+curl -o i-5.txt https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt
+curl -o i-6.txt https://raw.githubusercontent.com/Cats-Team/dns-filter/main/abp.txt
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
 cat i-mergd.txt | grep -v '^!' | grep -v '^！' | grep -v '^# ' | grep -v '^# ' | grep -v '^\[' | grep -v '^\【' | grep -v '^@' | grep -v '^@@' > i-tmpp.txt
